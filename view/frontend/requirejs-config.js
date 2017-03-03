629dda4e19e3628572dd68011ece18ce6474a88a
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2017-03-03 13:24:10 +0000

    Add image gallery

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,6 @@
 var config = {
     paths: {
         'quickview':       'Space48_QuickView/js/quickview',
-        'quickview-modal': 'Space48_QuickView/js/quickview-modal',
         'colorbox':        'Space48_QuickView/js/lib/jquery.colorbox-min'
     },
     map: {
--- view/frontend/web/js/quickview-modal.js
+++ view/frontend/web/js/quickview-modal.js
@@ -15,6 +15,8 @@ define([
             this.productName = ko.observable('');
             this.productSku = ko.observable('');
             this.isSalable = ko.observable('');
+            this.quantity = ko.observable(1);
+            this.imageIndex = ko.observable(0);
 
             this._super();
         },
@@ -24,11 +26,29 @@ define([
             return (this.isSalable() === '1') ? true : false;
         },
 
+        incrementQty: function () {
+            this.quantity(this.quantity() + 1);
+        },
+
+        decrementQty: function () {
+            if ( this.quantity() > 1 ) {
+                this.quantity(this.quantity() - 1);
+            }
+        },
+
+        thumbnailSelect: function (model, node) {
+            this.imageIndex($(node.delegateTarget).index());
+        },
+
         update: function (data) {
-            // Called from quickview.js on XHR response
+            // Called from quickview.js, on XHR response
             root.productName(data.name);
             root.productSku(data.sku);
             root.isSalable(data.is_salable);
+
+            // Defaults
+            root.imageIndex(0);
+            root.quantity(1);
         },
     });
 });
