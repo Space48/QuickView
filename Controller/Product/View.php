8efa7a32a47175c7a95df783d44974b04bb06561
Raul E Watson <rdiaz@Rauls-MacBook-Pro.local>
2017-03-06 14:31:00 +0000

    Add full Urls to images in gallery object

--- Controller/Product/View.php
+++ Controller/Product/View.php
@@ -13,32 +13,55 @@ declare(strict_types = 1);
 
 namespace Space48\QuickView\Controller\Product;
 
+use Magento\Catalog\Block\Product\ImageBuilder;
 use Magento\Catalog\Model\ProductRepository;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\Pricing\Helper\Data;
 
 class View extends Action
 {
 
+    /**
+     * @var
+     */
     protected $_resultJsonFactory;
+
+    /**
+     * @var ProductRepository
+     */
     protected $_productRepository;
+
     /**
      * @var Data
      */
     protected $_priceHelper;
 
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var ImageBuilder
+     */
+    private $_imageBuilder;
+
     public function __construct(
         Context $context,
-        Data $priceHelper,
-//    \Magento\Catalog\Block\Product\ListProduct,
-        ProductRepository $productRepository
+        StoreManagerInterface $storeManager,
+        ProductRepository $productRepository,
+        ImageBuilder $imageBuilder,
+        Data $priceHelper
     )
     {
+        $this->_storeManager = $storeManager;
         $this->_productRepository = $productRepository;
         $this->_priceHelper = $priceHelper;
         parent::__construct($context);
+        $this->_imageBuilder = $imageBuilder;
     }
 
     /**
@@ -61,7 +84,7 @@ class View extends Action
             'price'         => $this->_priceHelper->currency($product->getData('price'), true, false),
             'special_price' => ($product->getData('special_price')) ? $this->_priceHelper->currency($product->getData('special_price'), true, false) : null,
             'product_url'   => $product->getProductUrl(),
-            'gallery'       => $product->getData('media_gallery'),
+            'gallery'       => $this->getGallery($product),
             'breadcrumb'    => array()
         );
 
@@ -70,9 +93,30 @@ class View extends Action
         return $result;
     }
 
+    /**
+     * @param $productId
+     *
+     * @return \Magento\Catalog\Api\Data\ProductInterface|mixed
+     */
     private function getProductById($productId)
     {
         return $this->_productRepository->getById($productId);
+    }
+
+    /**
+     * @param $product
+     *
+     * @return mixed
+     */
+    protected function getGallery($product)
+    {
+        $gallery = $product->getData('media_gallery');
+        foreach ($gallery['images'] as $key => $image) {
+            $gallery['images'][$key]['file'] = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $image['file'];
+//            $gallery['images'][$key]['thumbnail'] = $this->_imageBuilder->setProduct($product)->setImageId('thumbnail')->setAttributes(array())->create();
+        }
+
+        return $gallery;
     }
 
 }
