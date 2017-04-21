267e11c3221e62040b9bce0be9e89307f0b4f322
Raul E Watson <raul@space48.com>
2017-04-21 05:21:33 +0100

    Fix AddToCart response broken after disable addToCart in admin

---
+++ Plugin/Checkout/Controller/Cart/Add.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Space48_QuickView
+ *
+ * @category    Space48
+ * @package     Space48_QuickView
+ * @Date        04/2017
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @author      @diazwatson
+ */
+
+namespace Space48\QuickView\Plugin\Checkout\Controller\Cart;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Add
+{
+
+    /**
+     * ScopeConfigInterface
+     *
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * Add constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Plugin
+     *
+     * @param \Magento\Checkout\Controller\Cart\Add $subject
+     * @param callable                              $proceed
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     */
+    public function aroundExecute(\Magento\Checkout\Controller\Cart\Add $subject, callable $proceed)
+    {
+        $referrerUrl = $subject->getRequest()->getServer('HTTP_REFERER');
+
+        $returnValue = $proceed();
+        if ($returnValue && !$this->shouldRedirectToCart()) {
+            $returnValue->setUrl($referrerUrl);
+        }
+
+        return $returnValue;
+    }
+
+    /**
+     * Should Redirect To Cart
+     *
+     * @return boolean
+     */
+    private function shouldRedirectToCart(): bool
+    {
+        return $this->scopeConfig->getValue(
+            'checkout/cart/redirect_to_cart',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+
+}
